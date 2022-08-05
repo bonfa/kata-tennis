6ac57b908fc0dbe588a2c10d27de93d312f07d09
ubunfu <bonfa@users.noreply.github.com>
2022-08-05 16:11:33 +0200

    [TENNIS_KATA] extract a function just to format the score

--- src/main/kotlin/it.fbonfadelli/TennisGame.kt
+++ src/main/kotlin/it.fbonfadelli/TennisGame.kt
@@ -1,7 +1,6 @@
 package it.fbonfadelli
 
 class TennisGame {
-    private val basicPoints = mapOf(0 to "Love", 1 to "15", 2 to "30", 3 to "40")
 
     private var playerBScore: Int = 0
     private var playerAScore: Int = 0
@@ -15,18 +14,23 @@ class TennisGame {
     }
 
     fun getScore(): String {
-        if (playerBScore > 3 && playerBScore - playerAScore > 1)
-            return "Player B win"
-        if (playerAScore > 3 && playerAScore - playerBScore > 1)
-            return "Player A win"
-        if (playerAScore > 3 && playerAScore - 1 == playerBScore)
-            return "Player A advantage"
-        if (playerBScore > 3 && playerBScore - 1 == playerAScore)
-            return "Player B advantage"
-        if (playerAScore == playerBScore && playerAScore > 2)
-            return "Deuce"
-        return basicPoints[playerAScore]!! + " - " + basicPoints[playerBScore]!!
+        return getScore2(playerAScore, playerBScore)
     }
 
-
+    companion object {
+        private val basicPoints = mapOf(0 to "Love", 1 to "15", 2 to "30", 3 to "40")
+        private fun getScore2(playerAScore: Int, playerBScore: Int): String {
+            if (playerBScore > 3 && playerBScore - playerAScore > 1)
+                return "Player B win"
+            if (playerAScore > 3 && playerAScore - playerBScore > 1)
+                return "Player A win"
+            if (playerAScore > 3 && playerAScore - 1 == playerBScore)
+                return "Player A advantage"
+            if (playerBScore > 3 && playerBScore - 1 == playerAScore)
+                return "Player B advantage"
+            if (playerAScore == playerBScore && playerAScore > 2)
+                return "Deuce"
+            return basicPoints[playerAScore]!! + " - " + basicPoints[playerBScore]!!
+        }
+    }
 }
