dd082b66ba3bd58ec9f41142ffd971dd05ca4f94
ubunfu <bonfa@users.noreply.github.com>
2022-08-05 16:22:35 +0200

    [TENNIS_KATA] replace chain of `if` with `when`

--- src/main/kotlin/it.fbonfadelli/TennisGame.kt
+++ src/main/kotlin/it.fbonfadelli/TennisGame.kt
@@ -16,22 +16,18 @@ class TennisGame {
     }
 
     companion object {
-        private fun getScore2(score: Score): String {
-            return if (score.playerBScore > 3 && score.playerBScore - score.playerAScore > 1)
-                "Player B win"
-            else if (score.playerAScore > 3 && score.playerAScore - score.playerBScore > 1)
-                "Player A win"
-            else if (score.playerAScore > 3 && score.playerAScore - 1 == score.playerBScore)
-                "Player A advantage"
-            else if (score.playerBScore > 3 && score.playerBScore - 1 == score.playerAScore)
-                "Player B advantage"
-            else if (score.playerAScore == score.playerBScore && score.playerAScore > 2)
-                "Deuce"
-            else {
-                val basicPoints = mapOf(0 to "Love", 1 to "15", 2 to "30", 3 to "40")
-                basicPoints[score.playerAScore]!! + " - " + basicPoints[score.playerBScore]!!
+        private fun getScore2(score: Score): String =
+            when {
+                score.playerBScore > 3 && score.playerBScore - score.playerAScore > 1 -> "Player B win"
+                score.playerAScore > 3 && score.playerAScore - score.playerBScore > 1 -> "Player A win"
+                score.playerAScore > 3 && score.playerAScore - 1 == score.playerBScore -> "Player A advantage"
+                score.playerBScore > 3 && score.playerBScore - 1 == score.playerAScore -> "Player B advantage"
+                score.playerAScore == score.playerBScore && score.playerAScore > 2 -> "Deuce"
+                else -> {
+                    val basicPoints = mapOf(0 to "Love", 1 to "15", 2 to "30", 3 to "40")
+                    basicPoints[score.playerAScore]!! + " - " + basicPoints[score.playerBScore]!!
+                }
             }
-        }
     }
 }
 
