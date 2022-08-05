749977877fbc2892ea8bd9cf7aa3878545ae588a
ubunfu <bonfa@users.noreply.github.com>
2022-08-05 14:44:11 +0200

    [TENNIS KATA] add test list

--- src/test/kotlin/it/fbonfadelli/HelloTest.kt
+++ src/test/kotlin/it/fbonfadelli/HelloTest.kt
@@ -3,6 +3,14 @@ package it.fbonfadelli
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+// Single Player
+// Love-15-30-40
+
+// Two players
+// deuce
+// advantage
+// win
+
 class HelloTest {
 
     @Test
